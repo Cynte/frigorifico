5517b697e786ddb83ecc18c5eec52900ee5ac2e9
leomilo <north.00262@gmail.com>
2024-01-22 22:07:38 -0300

    daleee

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import ChangingBackgrounds from "./Components/ChangingBackgrounds.jsx";
-import ChangingText from "./Components/ChangingText.jsx"
-import NavBar from "./Components/NavBar.jsx"
-import logo from "./Resources/Images/mainLogo2.png"
-import image from "./Resources/Images/ovinos-patagonia-6.jpg"
+import ChangingBackgrounds from './Components/ChangingBackgrounds.jsx';
+import ChangingText from './Components/ChangingText.jsx';
+import NavBar from './Components/NavBar.jsx';
+import logo from './Resources/Images/mainLogo2.png';
+import image from './Resources/Images/ovinos-patagonia-6.jpg';
+import './App.css';
 
-const images = [
-  image
-]
+const images = [image];
 
-function App({language}) {
+function App({ language }) {
   return (
     <div>
+      <NavBar text={language.navText} logo={logo} />
 
-      <NavBar text= {language.navText} logo={logo}/>
-
-      <div className="home">
-
-        <ChangingBackgrounds images={images}/>
-        <ChangingText text={language.homeText}/>
-
+      <div className='home'>
+        <ChangingBackgrounds images={images} />
+        <ChangingText text={language.homeText} />
       </div>
 
-        <div className="placeholder">SECCIÓN CON LO QUE GARANTIZA/OFRECE LA EMPRESA</div>
+      <div className='placeholder'>
+        SECCIÓN CON LO QUE GARANTIZA/OFRECE LA EMPRESA
+      </div>
 
-        <div className="placeholder">SECCIÓN SOBRE PRODUCTOS Y CALIDAD</div>
+      <div className='placeholder'>SECCIÓN SOBRE PRODUCTOS Y CALIDAD</div>
 
-        <div className="placeholder">SECCIÓN SOBRE LOGROS Y CRECIMIENTO</div>
+      <div className='placeholder'>SECCIÓN SOBRE LOGROS Y CRECIMIENTO</div>
 
-        <div className="placeholder">SECCIÓN DE PRENSA EN CASO DE HABER ARTÍCULOS</div>
+      <div className='placeholder'>
+        SECCIÓN DE PRENSA EN CASO DE HABER ARTÍCULOS
+      </div>
 
-        <div className="placeholder">SECCIÓN DE MAPAS Y MERCADOS</div>
+      <div className='placeholder'>SECCIÓN DE MAPAS Y MERCADOS</div>
 
-        <div className="placeholder">SECCIÓN DE PERSONAS DE LA EMPRESA, CONTACTO Y REDES</div>
-
+      <div className='placeholder'>
+        SECCIÓN DE PERSONAS DE LA EMPRESA, CONTACTO Y REDES
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import spanish from "./Resources/Languages/spanish.json"
-import english from "./Resources/Languages/english.json"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import spanish from './Resources/Languages/spanish.json';
+import english from './Resources/Languages/english.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const basename = process.env.PUBLIC_URL;
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Routes>
-        <Route path='/' element={<App language={spanish}/>}/>
-        <Route path='/en' element={<App language={english}/>}/>
+        <Route path='/' element={<App language={spanish} />} />
+        <Route path='/en' element={<App language={english} />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
